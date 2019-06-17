297a489b282cef9e1af2d04cc052375a6d47b557
KeefSteef <deazy01025@gmail.com>
2019-06-17 16:10:16 +0300

    DailyFunction

--- RemAI/Class1.cs
+++ RemAI/Class1.cs
@@ -11,7 +11,20 @@ namespace RemAI
         public static void toCollectDaily()
         {
 
-            Console.WriteLine("Test");
+            DateTime data_start = DateTime.Now, data_process;
+            Console.WriteLine("Now is " + data_start);
+            Console.WriteLine("Enter Data process ");
+            string s1 = Console.ReadLine();
+            data_process = Convert.ToDateTime(s1);
+            System.TimeSpan result = data_process - data_start;
+
+            Console.WriteLine("Осталось " + (data_process.Date - data_start.Date) + " Дней");
+            Console.WriteLine("За это время вы получите " + (data_process.DayOfYear - data_start.DayOfYear) * 25 + " очков");
+            Console.ReadKey();
+
+
         }
+
+       
     }
 }
