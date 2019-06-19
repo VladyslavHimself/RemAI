9c544613f8c9211b6eab939bc83e855737d6d75d
VladyslavHimself <vladyslavhimself@gmail.com>
2019-06-19 22:21:59 +0300

    Updated class info

--- RemAI/winrate.cs
+++ RemAI/winrate.cs
@@ -12,6 +12,19 @@ namespace RemAI
        
         public static void toCalculateWinrate()
         {
+
+            // #TODO Winrate algorithm
+
+            /*
+             *            Winrate Algorithm
+             * 
+             *  33 = 100 %
+             *  1  = 3 %
+             * 
+             * 
+             * 
+             */
+
             int absoluteWinrate = 100, nubmerOfColons = 33, lose = 0;
 
             Console.WriteLine($"Your winrate = {absoluteWinrate}");
