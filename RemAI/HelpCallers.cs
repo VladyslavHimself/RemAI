5f4f3f4a4e3589d85093b285186d95096e45f550
Vladyslav <vladyslavhimself@gmail.com>
2019-07-02 12:41:01 +0300

    Added new functions for commands

--- RemAI/HelpCallers.cs
+++ RemAI/HelpCallers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,18 @@ namespace RemAI
         public static void ShowMartengeilTutorial()
         {
 
+            Process.Start("https://github.com/VladyslavHimself/RemAI.git");
 
 
         }
 
         public static void ShowDailyRewardTutorial()
         {
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
 
 
 
@@ -26,7 +33,27 @@ namespace RemAI
         public static void ShowInfoAboutProgram()
         {
 
+            Console.WriteLine("--- Information about program ---\n");
+            Console.WriteLine(" Name: RemAI (bot) ");
+            Console.WriteLine(" Type: Legacy ");
+            Console.WriteLine(" Version: 1.2p");
+            Console.WriteLine(" You can check updates on -> https://github.com/VladyslavHimself/RemAI/releases");
 
+            Console.WriteLine("\n\t--- CONTACTS ---");
+            Console.WriteLine(" Program created by VladyslavHimself.");
+            Console.WriteLine(" Contact info -> vladyslavhimself@gmail.com");
+
+
+            
+            Console.WriteLine("\n---------------------------------\n");
+
+
+        }
+
+        public static void DownloadRelease()
+        {
+            Console.WriteLine("Open Release page!");
+            Process.Start("https://github.com/VladyslavHimself/RemAI/releases");
 
         }
 
